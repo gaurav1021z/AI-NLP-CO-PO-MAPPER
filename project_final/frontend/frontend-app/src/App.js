ec803e22def5ad8a9cced43c2f264eeac6751fd0
Gaurav Zambare <gauravzambre143@gmail.com>
2026-08-11 16:13:00 +0530

    Fix login redirect to existing dashboard

--- project_final/frontend/frontend-app/src/App.js
+++ project_final/frontend/frontend-app/src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 function App() {
-  const path = window.location.pathname;
-
-  if (path === "/" || path === "/login") {
-    return <div />;
-  }
-
-  window.location.replace("/login");
-  return null;
+  return <div />;
 }
 
 export default App;
